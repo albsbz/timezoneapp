acb5373785d5502d472e1f8352b5d342d7600de4
albsbz <best.shop.bot@gmail.com>
2020-10-13 20:58:19 +0300

    v0.1

---
+++ src/components/TimeInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <input class="input is-hovered time" type="text" placeholder="Time" maxlength="5" 
+        :value="timeShow" 
+        @keydown.prevent="updateTime"
+        @clear="clearInput"
+        >
+</template>
+
+<script>
+export default {
+  name: 'TimeInput',
+  data: function () {return {
+      time: [undefined,undefined,undefined,undefined]
+    }
+  },
+  computed: {
+    timeShow: function(){
+      let newArr=[...this.time];
+      newArr.splice(2, 0, ':');
+      return newArr.join('')
+    },    
+  },
+  methods: {
+    updateTime: function(e){
+      if ((e.keyCode>47&&e.keyCode<58)||(e.keyCode>95&&e.keyCode<106)) {
+        this.time.push(e.key);
+        this.time=this.time.slice(-4);
+      }
+    },
+    giveTime: function(){
+        let hours = this.time[0]+this.time[1];
+        let minutes = this.time[2]+this.time[3];
+        return (hours>24||minutes>60)?false:{hours, minutes}      
+    },
+    clearInput: function(){
+      this.time=[undefined,undefined,undefined,undefined];
+    }
+    
+  }
+}
+</script>
+
+<style scoped>
+    .time{
+        text-align: center;
+    }
+</style>
--- src/components/TimezoneApp.vue
+++ src/components/TimezoneApp.vue
@@ -7,23 +7,27 @@
         <p class="title is-3">My Timezone</p>
         <div class="field">
           <label class="label">Select your Timezone</label>
-          <div class="control" v-if="ifShowYouControl">
+          <div class="control">
             <div class="select">
-              <select>
+              <select v-model="myTimezone">
                 <option v-for="timezone in timezones" 
-                        value="timezone" 
-                        v-bind:key="timezone">
+                        :value="timezone" 
+                        :key="timezone">
                         UTC {{timezone}}
                 </option>
               </select>
             </div>
           </div>
           <label class="label">Your time</label>
-          <div class="control">
-            <input class="input is-hovered time" type="text" placeholder="Time" maxlength="5" 
-                  :value="myTimeShow" 
-                  @keydown.prevent="updateMyTime"
-                  @focus="ifShowPartnerControl=false">
+          <div class="control" v-show="ifShowYouControl"> 
+            <time-input
+              ref="myTimer" 
+              @focus.native="ifShowPartnerControl=false"
+              @keyup.native="isTimeValid"
+            />
+          </div>
+          <div v-show="!ifShowYouControl">
+            {{myAppointedTime?myAppointedTime.hours:''}}:{{myAppointedTime?myAppointedTime.minutes:''}}
           </div>
         </div> 
       </div>
@@ -33,21 +37,26 @@
         <div class="field">
           <label class="label">Select partner Timezone</label>
           <div class="control">
-            <div class="select">
-              <select>
+            <div class="select" >
+              <select v-model="partnerTimezone">
                 <option v-for="timezone in timezones" 
-                        value="timezone" 
-                        v-bind:key="timezone">
+                        :value="timezone" 
+                        :key="timezone">
                         UTC {{timezone}}
                 </option>
               </select>
             </div>
           </div>
           <label class="label">Partner time</label>
-          <div class="control" v-if="ifShowPartnerControl">
-            <input class="input is-hovered time" type="text" placeholder="Time" maxlength="5" 
-                  :value="partnerTimeShow" 
-                  @keydown.prevent="updatePartnerTime">
+          <div class="control" v-show="ifShowPartnerControl">
+            <time-input
+              ref="partnerTimer"
+              @focus.native="ifShowYouControl=false"
+              @keyup.native="isTimeValid"
+            />
+          </div>
+          <div v-show="!ifShowPartnerControl">
+            {{partnerAppointedTime?partnerAppointedTime.hours:''}}:{{partnerAppointedTime?partnerAppointedTime.minutes:''}}
           </div>
         </div>
 
@@ -58,7 +67,7 @@
 
     <div class="field is-grouped control-wrapper">
       <div class="control">
-        <button class="button is-link">Schedule</button>
+        <button class="button is-link" @click="schedule" :disabled="scheduleDisabled">Schedule</button>
       </div>
       <div class="control">
         <button class="button is-link is-light" @click="clearForm">Clear</button>
@@ -71,41 +80,78 @@
 </template>
 
 <script>
+import TimeInput from './TimeInput.vue'
+
 export default {
   name: 'TimezoneApp',
+  components: {
+    TimeInput
+  },
   data: function () {return {
       timezones: [-11,-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      myTime: [0,0,0,0],
-      partnerTime: [0,0,0,0],
+      // myTime: [0,0,0,0],
+      // partnerTime: [0,0,0,0],
+      myTimezone: 0,
+      partnerTimezone: 0,
+      myAppointedTime: undefined,
+      partnerAppointedTime: undefined,
       ifShowPartnerControl: true,
       ifShowYouControl: true,
+      scheduleDisabled:false,
     }
   },
   computed: {
-    myTimeShow: function(){
-      let newArr=[...this.myTime];
-      newArr.splice(2, 0, ':');
-      return newArr.join('')
-    },
-    partnerTimeShow: function(){
-      let newArr=[...this.partnerTime];
-      newArr.splice(2, 0, ':');
-      return newArr.join('')
-    }
+   
     
   },
   methods: {
-    updateMyTime: function(e){
-      if ((e.keyCode>47&&e.keyCode<58)||(e.keyCode>95&&e.keyCode<106)) {
-        this.myTime.push(e.key);
-        this.myTime=this.myTime.slice(-4);
-      }
-    },
+   
     clearForm: function(){
-      this.myTime=[0,0,0,0];
-      this.partnerTime=[0,0,0,0];
       this.ifShowPartnerControl=true;
       this.ifShowYouControl=true;
+      this.$refs.myTimer.clearInput();
+      this.$refs.partnerTimer.clearInput();
+      this.scheduleDisabled=false; 
+      this.myAppointedTime= undefined;
+      this.partnerAppointedTime=undefined;     
+    },
+    schedule: function(){
+      let time=this.isTimeValid();
+      if (time){
+        if (!isNaN(time.myTime.hours)){
+          this.partnerAppointedTime={
+            hours: (time.myTime.hours-this.myTimezone+this.partnerTimezone)%24,
+            minutes: time.myTime.minutes
+          }
+          this.myAppointedTime= undefined;
+        } else {
+          this.myAppointedTime={
+            hours: (time.partnerTime.hours-this.partnerTimezone+this.myTimezone)%24,
+            minutes: time.partnerTime.minutes
+          }
+          this.partnerAppointedTime= undefined;
+        }
+      }
+
+    },
+    isTimeValid: function(){
+      let myTime=this.$refs.myTimer.giveTime();
+      let partnerTime=this.$refs.partnerTimer.giveTime();
+      if (myTime===false) {
+        this.$refs.myTimer.$el.focus();
+        this.scheduleDisabled=true;
+        return false
+      }
+      else if (partnerTime===false) {
+        this.$refs.partnerTimer.$el.focus();
+        this.scheduleDisabled=true;
+        return false
+      }
+      else {
+        console.log({myTime, partnerTime})
+        this.scheduleDisabled=false;
+        return {myTime, partnerTime}
+      }
     }
   }
 }

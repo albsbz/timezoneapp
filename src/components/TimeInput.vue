bcba8ee924f50800aaeea92e0dabb4c02f47816f
albsbz <best.shop.bot@gmail.com>
2020-10-14 08:17:23 +0300

    backspace not work

--- src/components/TimeInput.vue
+++ src/components/TimeInput.vue
@@ -26,8 +26,13 @@ export default {
         this.time.push(e.key);
         this.time=this.time.slice(-4);
       }
+    if ((e.keyCode===8)) {
+        this.time.pop();
+      }
     },
     giveTime: function(){
+        if (this.time[0]===undefined) {this.time[0]=0}
+        if (this.time[3]===undefined) {this.time[3]=0}
         let hours = Number(this.time[0]+this.time[1]);
         let minutes = Number(this.time[2]+this.time[3]);
         return (hours>24||minutes>60)?false:{hours, minutes}      
